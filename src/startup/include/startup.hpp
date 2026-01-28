93ea34dda87c4dffe42fe9076180671eda4ad745
JustinShih <justinshih@gapp.nthu.edu.tw>
2026-01-29 01:02:26 +0800

    feat: start refactor, new encoding rules - robot_config.yaml for team color and initial pose - robot and plan get from launch file or web service - state machine refactor - OOP version

---
+++ src/startup/include/startup.hpp
@@ -0,0 +1,120 @@
+/* rclcpp publisher */
+#include "rclcpp/rclcpp.hpp"
+#include "std_srvs/srv/set_bool.hpp"
+#include "std_srvs/srv/trigger.hpp"
+#include "std_msgs/msg/bool.hpp"
+#include "std_msgs/msg/int32.hpp"
+#include "std_msgs/msg/int16.hpp"
+#include "std_msgs/msg/float32.hpp"
+#include "std_msgs/msg/string.hpp"
+#include "std_msgs/msg/int32_multi_array.hpp"
+#include "geometry_msgs/msg/point_stamped.hpp"
+#include "geometry_msgs/msg/pose_with_covariance_stamped.hpp"
+#include "btcpp_ros2_interfaces/srv/start_up_srv.hpp" 
+
+// tf2 
+#include <tf2/LinearMath/Quaternion.h>
+#include <tf2/LinearMath/Vector3.h>
+#include <tf2_geometry_msgs/tf2_geometry_msgs.hpp>
+#include <tf2/impl/utils.h>
+#include <tf2_ros/transform_listener.h>
+#include <tf2_ros/buffer.h>
+#include <tf2/exceptions.h>
+
+#include <jsoncpp/json/json.h>
+#include <fstream>
+#include <iostream>
+#include <stdexcept>
+#include <stdio.h>
+#include <string>
+
+#include "btcpp_ros2_interfaces/msg/obstacles.hpp"
+#include "btcpp_ros2_interfaces/msg/circle_obstacle.hpp"
+#include "btcpp_ros2_interfaces/msg/segment_obstacle.hpp"
+
+#include "bt_config.hpp"
+
+#define PI 3.1415926
+
+enum class StartUpState {
+    INIT,
+    READY,
+    START,
+    END,
+    ERROR
+};
+
+class StartUp : public rclcpp::Node {
+
+public:
+    // constructor
+    StartUp();
+    void initParam();
+
+    // control flow
+    void stateTransition();
+
+    // init phase
+    void getWebPlan();
+    void parsePlanCode();
+
+    // ready phase
+    void publishSystemCheckSignal();
+    void systemCheckFeedback(const std::shared_ptr<btcpp_ros2_interfaces::srv::StartUpSrv::Request> request,
+        std::shared_ptr<btcpp_ros2_interfaces::srv::StartUpSrv::Response> response);
+    bool isAllSystemReady();
+    void startCallback();
+
+    // start phase
+    void publishStartSignal();
+    void tickSima(double game_time);
+    void publishTime();
+    bool gameOver(double game_time);
+
+private:
+    // game timer
+    rclcpp::TimerBase::SharedPtr timer;
+    rclcpp::Publisher<std_msgs::msg::Float32>::SharedPtr game_time_pub;
+    rclcpp::Rate rate;
+
+    // State checker for other groups
+    rclcpp::Publisher<std_msgs::msg::Bool>::SharedPtr are_you_ready_pub;   
+    rclcpp::Publisher<std_msgs::msg::String>::SharedPtr plan_file_pub;
+    rclcpp::Service<btcpp_ros2_interfaces::srv::StartUpSrv>::SharedPtr ready_srv_server;
+    rclcpp::Subscription<std_msgs::msg::Bool>::SharedPtr plug_sub;
+    rclcpp::Publisher<geometry_msgs::msg::PoseWithCovarianceStamped>::SharedPtr initialpose_pub;
+
+    // Sima
+    // 2026 Eurobot game have two types of sima
+    // sima will be start by a int topic for potiential plan selection
+    rclcpp::Publisher<std_msgs::msg::Int16>::SharedPtr start_sima_pub;
+    rclcpp::Publisher<std_msgs::msg::Int16>::SharedPtr start_ninja_pub;
+    int sima_tick_threshold;
+    int sima_selected_plan;
+    bool sima_started;
+
+    // web
+    // robot computer given a web interface for plan selection
+    // web interface will help select team, plan, two digit message
+    // for example: 40 -> 4th plan of Yellow
+    //              31 -> 3th plan of Blue
+    rclcpp::Client<std_srvs::srv::Trigger>::SharedPtr web_plan_client;
+    bool web_request_received;
+
+    // program variables
+    Team team; // came from param, can be modify by web interface
+    Robot robot; // came from param
+    int selected_plan; // came from plan code
+    std::string plan_file_name; // parse by plan code, robot name
+
+    StartUpState startup_state;
+    geometry_msgs::msg::PoseWithCovarianceStamped start_position;
+    double start_time;
+    bool is_plugged;
+    int group_state[5];
+
+    // Parameters
+    std::string robot_name;
+    std::vector<double> blue_start_pose;
+    std::vector<double> yellow_start_pose;
+};
---
+++ src/startup/src/startup_new.cpp
@@ -0,0 +1,217 @@
+#include "startup.hpp"
+
+const int TIME_RATE = 100;
+const int SIMA_TICK_THRESHOLD = 85;
+const int GAME_TIME = 100; 
+
+StartUp::StartUp() : Node("startup_node"){
+    // game timer
+    timer = this->create_wall_timer(
+        std::chrono::microseconds(TIME_RATE),
+        std::bind(&StartUp::stateTransition, this));
+
+    game_time_pub = this->create_publisher<std_msgs::msg::Float32>("/robot/startup/game_time", 2);
+    rate = rclcpp::Rate(TIME_RATE);
+
+    // State checker for other groups
+    are_you_ready_pub = this->create_publisher<std_msgs::msg::Bool>("/robot/startup/are_you_ready", 2);
+    ready_srv_server = this->create_service<btcpp_ros2_interfaces::srv::StartUpSrv>(
+        "/robot/startup/ready_signal", std::bind(&StartUp::systemCheckFeedback, this, std::placeholders::_1, std::placeholders::_2));
+    plug_sub = this->create_subscription<std_msgs::msg::Bool>("/robot/startup/plug", 2, std::bind(&StartUp::startCallback, this, std::placeholders::_1));
+    plan_file_pub = this->create_publisher<std_msgs::msg::String>("/robot/startup/plan_file", 2);
+
+    // sima
+    start_sima_pub = this->create_publisher<std_msgs::msg::Int16>("/sima/start", 2);
+    start_ninja_pub = this->create_publisher<std_msgs::msg::Int16>("/ninja/start", 2);
+    sima_tick_threshold = SIMA_TICK_THRESHOLD;
+    sima_selected_plan = 0;
+    sima_started = false;
+
+    // web
+    web_plan_client = this->create_client<std_srvs::srv::Trigger>("/robot/startup/web_plan");
+    web_request_received = false;
+
+    // default variable value
+    team = Team::YELLOW;
+    robot = Robot::WHITE;
+    selected_plan = 0;
+    plan_file_name = "";
+    startup_state = StartUpState::INIT;
+    start_position = geometry_msgs::msg::PoseWithCovarianceStamped();
+    group_state = {0, 0, 0, 0, 0};
+    is_plugged = false;
+    initParam();
+}
+
+void StartUp::initParam() {
+    this->declare_parameter<std::string>("robot_name", "White");
+    this->declare_parameter<std::vector<double>>("blue_team.start_pose", std::vector<double>{0.0, 0.0, 0.0});
+    this->declare_parameter<std::vector<double>>("yellow_team.start_pose", std::vector<double>{0.0, 0.0, 0.0});
+
+    this->get_parameter("robot_name", robot_name);
+    this->get_parameter("blue_team.start_pose", blue_start_pose);
+    this->get_parameter("yellow_team.start_pose", yellow_start_pose);
+
+    RCLCPP_INFO(this->get_logger(), "[StartUp]: Loaded config for robot: %s", robot_name.c_str());
+}
+
+void StartUp::stateTransition() {
+    switch(startup_state) {
+        case StartUpState::INIT:
+            getWebPlan();
+            parsePlanCode();
+            startup_state = StartUpState::READY;
+            break;
+        case StartUpState::READY:
+            publishSystemCheckSignal();
+            if(isAllSystemReady()) {
+                publishStartSignal();
+                startup_state = StartUpState::START;
+            }
+            break;
+        case StartUpState::START:
+            publishTime();
+            if(gameOver()) startup_state = StartUpState::END;
+            break;
+        case StartUpState::END:
+            RCLCPP_INFO(this->get_logger(), "[StartUp]: End state");
+            break;
+        case StartUpState::ERROR:
+            RCLCPP_ERROR(this->get_logger(), "[StartUp]: Error state");
+            break;
+        default:
+            RCLCPP_ERROR(this->get_logger(), "[StartUp]: Unknown state");
+            break;
+    }
+}
+
+void StartUp::getWebPlan() {
+    if(!web_plan_client->wait_for_service(std::chrono::seconds(1))) {
+        RCLCPP_ERROR(this->get_logger(), "Service not available, use default plan");
+        selected_plan = 11;
+        return;
+    }
+    auto request = std::make_shared<std_srvs::srv::Trigger::Request>();
+    web_request_received = true;
+    web_plan_client->async_send_request(
+        request,
+        [this](rclcpp::Client<std_srvs::srv::Trigger>::SharedFuture future){
+            try {
+                auto response = future.get();
+                if(response->success){
+                    selected_plan = std::stoi(response->message);
+                    RCLCPP_INFO(this->get_logger(), "[StartUp]: Received relected plan: %d", selected_plan);
+                } else {
+                    RCLCPP_ERROR(this->get_logger(), "[StartUp]: Failed to get plan: %s", response->message.c_str());
+                    web_request_received = false;
+                }
+            } catch (const std::exception& e) {
+                RCLCPP_ERROR(this->get_logger(), "[StartUp]: Failed to get plan: %s", e.what());
+                web_request_received = false;
+            }
+        }
+    );
+    // TODO: get Sima plan from defined config file
+    sima_selected_plan = selected_plan; // temporary
+}
+
+void StartUp::parsePlanCode() {
+    // get info
+    robot = stringToRobot(robot_name);
+    // Assuming last digit 0 = Yellow, 1 = Blue to match enum (Yellow=0, Blue=1)
+    team = static_cast<Team>(selected_plan % 10);
+    selected_plan = selected_plan / 10;
+
+    // translate plan code and other info into plan file name
+    // Format: "robot_team_planNumber" e.g. "white_yellow_1.xml" 
+    plan_file_name = robotToString(robot) + "_" + teamToString(team) + "_" + std::to_string(selected_plan) + ".xml";
+    RCLCPP_INFO(this->get_logger(), "[StartUp]: Parsed Plan: %s", plan_file_name.c_str());
+
+    // publish plan file
+    auto msg = std_msgs::msg::String();
+    msg.data = plan_file_name;
+    plan_file_pub->publish(msg);
+}
+
+void StartUp::publishSystemCheckSignal() {
+    auto msg = std_msgs::msg::Bool();
+    msg.data = true;
+    are_you_ready_pub->publish(msg);
+}
+
+void StartUp::systemCheckFeedback(const std::shared_ptr<btcpp_ros2_interfaces::srv::StartUpSrv::Request> request,
+    std::shared_ptr<btcpp_ros2_interfaces::srv::StartUpSrv::Response> response) {
+    RCLCPP_INFO(this->get_logger(), "[StartUp]: Received system check feedback from group %d, state: %d", request->group_id, request->state);
+    group_state[request->group_id] = request->state;
+    response->group = request->group;
+    response->success = true;
+    switch(request->group){
+        case 1: // main
+            if(request->state == 1) { group_state[1] = 1; RCLCPP_INFO(this->get_logger(), "[StartUp]: Main system is ready");}
+            break;
+        case 2: // vision
+            if(request->state == 1) { group_state[2] = 1; RCLCPP_INFO(this->get_logger(), "[StartUp]: Vision system is ready");}
+            break;
+        case 3: // navigation
+            if(request->state == 1) { group_state[3] = 1; RCLCPP_INFO(this->get_logger(), "[StartUp]: Navigation system is ready");}
+            break;
+        case 4: // localization
+            if(request->state == 1) { group_state[4] = 1; RCLCPP_INFO(this->get_logger(), "[StartUp]: Localization system is ready");}
+            break;
+        // TODO: add more group by increase group_state size
+    }
+    RCLCPP_INFO(this->get_logger(), "[StartUp]: Response %d to %d", response->group, response->success);
+}
+
+bool StartUp::isAllSystemReady() {
+    for(int i = 1; i < group_state.size(); i++) {
+        if(group_state[i] != 1) {
+            group_state[0] = 0;
+            return false;
+        }
+    }
+    group_state[0] = 1;
+    if(is_plugged && group_state[0] == 1) {
+        RCLCPP_INFO(this->get_logger(), "[StartUp]: All system is ready & Received start signal");
+        return true;
+    }
+    return false;
+}
+
+void StartUp::startCallback(const std::shared_ptr<std_msgs::msg::Bool> msg) {
+    if(startup_state == StartUpState::READY) {
+        is_plugged = msg->data;
+    }
+}
+
+void StartUp::publishStartSignal() {
+    auto msg = std_msgs::msg::Bool();
+    msg.data = true;
+    start_signal_pub->publish(msg);
+    
+}
+
+void StartUp::tickSima(double game_time) {
+    if(game_time >= sima_tick_threshold && !sima_started) {
+        auto msg = std_msgs::msg::Int16();
+        msg.data = sima_selected_plan;
+        start_sima_pub->publish(msg);
+        sima_started = true;
+
+        // TODO: add continuous publishing if sima is not started for one time publish
+        // add after testing sima
+    }
+}
+
+void StartUp::publishTime() {
+    double cur_time = this->get_clock()->now().seconds();
+    std_msgs::msg::Float32 cur_time_msg;
+    cur_time_msg.data = cur_time - start_time;
+    game_time_pub->publish(cur_time_msg);
+    tickSima(cur_time_msg.data);
+}
+
+bool StartUp::gameOver(double game_time) {
+    if(game_time >= GAME_TIME) return true;
+    return false;
+}
