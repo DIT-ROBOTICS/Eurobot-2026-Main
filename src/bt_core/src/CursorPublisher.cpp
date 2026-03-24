48fd002d8e0006c53097b5e621a40ed640f92db8
chuangtsh <chuangtsh0526@gmail.com>
2026-03-25 01:02:44 +0800

    feat: test CursorSubTree

--- src/bt_core/src/CursorPublisher.cpp
+++ src/bt_core/src/CursorPublisher.cpp
@@ -18,49 +18,68 @@ CursorPublisher::CursorPublisher(const std::string& name, const BT::NodeConfig& 
 
 BT::PortsList CursorPublisher::providedPorts() {
     return {
-        BT::InputPort<std::string>("direction", "left", "Direction: 'left' or 'right'"),
-        BT::InputPort<bool>("value", false, "Bool value to publish")
+        BT::InputPort<std::string>("arms", "left", "arms: 'left' or 'right'"),
+        BT::InputPort<bool>("value", false, "Bool value to publish"),
     };
 }
 
 BT::NodeStatus CursorPublisher::onStart() {
     // Read input ports
-    std::string direction;
-    if (!getInput<std::string>("direction", direction)) {
-        CP_WARN(node_, "Missing direction, defaulting to 'left'");
-        direction = "left";
+    if (!getInput<std::string>("arms", arms_)) {
+        CP_WARN(node_, "Missing arms, defaulting to 'left'");
+        arms_ = "left";
     }
 
-    bool value;
-    if (!getInput<bool>("value", value)) {
+    if (!getInput<bool>("value", value_)) {
         CP_WARN(node_, "Missing value, defaulting to false");
-        value = false;
+        value_ = false;
     }
-
-    // Create message
-    std_msgs::msg::Bool msg;
-    msg.data = value;
-
-    // Publish based on direction
-    if (direction == "left") {
-        CP_INFO(node_, "Publishing to /robot/on_cursor_left: %s", value ? "true" : "false");
-        left_pub_->publish(msg);
-    } else if (direction == "right") {
-        CP_INFO(node_, "Publishing to /robot/on_cursor_right: %s", value ? "true" : "false");
-        right_pub_->publish(msg);
-    } else {
-        CP_WARN(node_, "Invalid direction '%s', not publishing", direction.c_str());
-        return BT::NodeStatus::FAILURE;
-    }
-
+    
     return BT::NodeStatus::RUNNING;
 }
 
 BT::NodeStatus CursorPublisher::onRunning() {
     // Immediately succeed after publishing
-    return BT::NodeStatus::SUCCESS;
+    if(isArriveGoalPose()){
+        // Create message
+        std_msgs::msg::Bool msg;
+        msg.data = value_;
+
+        // Publish based on arms
+        if (arms_ == "left") {
+            CP_INFO(node_, "Publishing to /robot/on_cursor_left: %s", value_ ? "true" : "false");
+            left_pub_->publish(msg);
+        } else if (arms_ == "right") {
+            CP_INFO(node_, "Publishing to /robot/on_cursor_right: %s", value_ ? "true" : "false");
+            right_pub_->publish(msg);
+        } else {
+            CP_WARN(node_, "Invalid arms '%s', not publishing", arms_.c_str());
+            return BT::NodeStatus::FAILURE;
+        }
+        return BT::NodeStatus::SUCCESS;
+    }
+    return BT::NodeStatus::RUNNING;
 }
 
 void CursorPublisher::onHalted() {
     CP_INFO(node_, "Halted (preempted)");
+}
+
+bool CursorPublisher::isArriveGoalPose()
+{
+    geometry_msgs::msg::PoseStamped robot_pose_;
+
+    if (!blackboard_->get("robot_pose", robot_pose_)) {
+        CP_WARN(node_, "No robot_pose in blackboard");
+        return false;
+    }
+
+    double y = robot_pose_.pose.position.y;
+
+    // CP_INFO(node_, "Current y: %.3f", y);
+
+    const double target_y = 0.2;
+    const double tolerance = 0.15;
+
+    return std::abs(y - target_y) < tolerance;
 }
