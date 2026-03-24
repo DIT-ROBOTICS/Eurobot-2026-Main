e2adf1f5c835476ce8ba1d12c64bd171bf22059c
chuangtsh <chuangtsh0526@gmail.com>
2026-03-25 01:02:44 +0800

    feat: test CursorSubTree

--- src/bt_core/include/CursorPublisher.hpp
+++ src/bt_core/include/CursorPublisher.hpp
@@ -5,7 +5,10 @@
 #include "behaviortree_cpp/bt_factory.h"
 #include "rclcpp/rclcpp.hpp"
 #include "std_msgs/msg/bool.hpp"
+#include "geometry_msgs/msg/pose_stamped.hpp"
+
 #include <string>
+#include <memory>
 
 using namespace BT;
 using namespace std;
@@ -13,16 +16,21 @@ using namespace std;
 /**
  * @brief CursorPublisher - StatefulActionNode that publishes a cursor command to firmware.
  *
- * Publishes to /robot/on_cursor_left or /robot/on_cursor_right (Bool).
+ * Publishes to:
+ *   /robot/on_cursor_left
+ *   /robot/on_cursor_right
  *
  * Input ports:
- *   - direction: "left" or "right" to choose the topic
- *   - value: bool value to publish (true or false)
+ *   - arms: "left" or "right"
+ *   - value: bool value to publish
+ *   - target_y: target y position
  */
 class CursorPublisher : public BT::StatefulActionNode {
 public:
-    CursorPublisher(const std::string& name, const BT::NodeConfig& config,
-                    const RosNodeParams& params, BT::Blackboard::Ptr blackboard);
+    CursorPublisher(const std::string& name,
+                    const BT::NodeConfig& config,
+                    const RosNodeParams& params,
+                    BT::Blackboard::Ptr blackboard);
 
     static BT::PortsList providedPorts();
 
@@ -31,15 +39,25 @@ public:
     void onHalted() override;
 
 private:
-    // Publishers
+    // ===== ROS =====
     rclcpp::Publisher<std_msgs::msg::Bool>::SharedPtr left_pub_;
     rclcpp::Publisher<std_msgs::msg::Bool>::SharedPtr right_pub_;
-
-    // Node reference
     std::shared_ptr<rclcpp::Node> node_;
 
-    // Blackboard reference
+    // ===== Blackboard =====
     BT::Blackboard::Ptr blackboard_;
+
+    // ===== Input parameters =====
+    std::string arms_;
+    bool value_;
+    double target_y_;  
+    double tolerance_;  
+
+    // ===== Internal state =====
+    bool published_; 
+
+    // ===== Functions =====
+    bool isArriveGoalPose();
 };
 
 #endif // CURSOR_PUBLISHER_HPP
